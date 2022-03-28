62798764f4fb5b9d2b739ea6183ff4c31e17e793
PitKoro <pit.koro@gmail.com>
2022-03-28 12:51:12 +0000

    added lab1

---
+++ lab1/commands.sql
@@ -0,0 +1,17 @@
+use stud;
+
+CREATE TABLE `Person` (
+  `login` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `age` int NOT NULL,
+  CHECK (`age`>=0)
+);
+
+INSERT INTO `Person` (`login`, `first_name`, `last_name`, `age`) VALUES
+('admin', 'Foo', 'Bar', 42),
+('manager', 'Brew', 'Spencer', 22),
+('adam', 'Adam', 'Smith', 16);
+
+ALTER TABLE `Person`
+  ADD PRIMARY KEY (`login`);
